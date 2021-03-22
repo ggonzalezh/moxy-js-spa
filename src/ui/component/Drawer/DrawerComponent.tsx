83c553e99e517347f96ab8aaba5d42a946b1aacb
ggonzalezh <ggonzalezh15@gmail.com>
2021-03-21 22:03:38 -0300

    (:construction:) Drawer Component - Work in Progress

---
+++ src/ui/component/Drawer/DrawerComponent.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Drawer } from "antd";
+
+export const DrawerComponent = ({ showDrawer }: DrawerComponentProps) => {
+  <Drawer></Drawer>;
+};
+
+export interface DrawerComponentProps {
+  showDrawer: boolean;
+}
